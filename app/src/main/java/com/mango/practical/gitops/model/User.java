034747e34f71867452882659a44326b3e43295ef
Rajendra Maruthi Mahawadi <12994577+therainsofcastamere@users.noreply.github.com>
2024-07-30 22:53:26 +1200

    first real commit

---
+++ app/src/main/java/com/mango/practical/gitops/model/User.java
@@ -0,0 +1,65 @@
+package com.mango.practical.gitops.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotBlank(message = "Name is mandatory")
+    @Column(name = "name")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Column(name = "email")
+    @Email(message = "Not an Email")
+    private String email;
+
+    @Column(name = "phone_no")
+    private long phoneNo;
+
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public long getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(long phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+}
---
+++ app/src/test/java/com/mango/practical/gitops/GitopsApplicationTests.java
@@ -0,0 +1,13 @@
+// package com.mango.practical.gitops;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class GitopsApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
